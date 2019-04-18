221f68e8a72ae3b99e282167197c8b68ba0c0527
Bojidar Aleksandrov <bojidar.z.aleksandrov@gmail.com>
2019-04-18 10:45:27 +0300

    Modified SumString and ConsoleReader
    Changed System.errs to System.outs everywhere
    Added termination string in ConsoleReader instread
    of hardcoded "stop"
    Changed SumString.sum to rethrow exceptions with better message
    Updated main to try/catch unchecked exception
    Changed documentation on ConsoleReader to be accurate

--- src/com/clouway/ConsoleReaderInBounds.java
+++ src/com/clouway/ConsoleReaderInBounds.java
@@ -11,54 +11,56 @@ public class ConsoleReaderInBounds {
     /**
      * Reads from console, parses it to int and compares it to leftBound and rightBound
      * Breaks if console input is not parsable integer or input is out of bounds
+     * Stops when terminationString is submitted
      * @param leftBound lower border
      * @param rightBound higher border
      * @throws InputOutOfBoundsException input is outside borders
      */
-    public void readInBounds(int leftBound, int rightBound) throws InputOutOfBoundsException {
-        try (Scanner scan = new Scanner(System.in)) {
+    public void readInBounds(int leftBound, int rightBound, String terminationString) throws InputOutOfBoundsException {
+        Scanner scan = new Scanner(System.in);
+        try {
             String nextLine;
             int input;
             do {
                 nextLine = scan.nextLine();
-                if (nextLine.equals("stop"))
+                if (nextLine.equals(terminationString))
                     break;
                 input = Integer.parseInt(nextLine);
                 System.out.println(input);
             } while (leftBound <= input && input <= rightBound);
-            if (!nextLine.equals("stop"))
+            if (!nextLine.equals(terminationString))
                 throw new InputOutOfBoundsException("Number is out of bounds");
         } catch (NumberFormatException e) {
-            System.err.println(e.getMessage() + " is not a parsable integer");
+            System.out.println(e.getMessage() + " is not a parsable integer");
         }
 
     }
 
     /**
      * Reads from console, parses it to int and compares it to leftBound and rightBound
-     * Stops reading when "stop" is submitted
+     * Stops reading when terminationString is submitted
      * @param leftBound lower border
      * @param rightBound higher border
      */
-    public void anotherReadInBounds(int leftBound, int rightBound) {
-        try (Scanner scan = new Scanner(System.in)) {
-            String nextLine;
-            int input;
-            do {
-                nextLine = scan.nextLine();
-                try {
-                    input = Integer.parseInt(nextLine);
-                    if(leftBound > input || input > rightBound){
-                        throw new InputOutOfBoundsException("Input is out of bounds");
-                    }else
-                        System.out.println(input);
-                }catch (InputOutOfBoundsException e){
-                    System.err.println(e.getMessage());
-                }catch (NumberFormatException e){
-                    if(!nextLine.equals("stop"))
-                        System.err.println(e.getMessage() + " is not a parsable integer");
-                }
-            }while (!nextLine.equals("stop"));
-        }
+    public void anotherReadInBounds(int leftBound, int rightBound, String terminationString) {
+        Scanner scan = new Scanner(System.in);
+        String nextLine;
+        int input;
+        do {
+            nextLine = scan.nextLine();
+            try {
+                input = Integer.parseInt(nextLine);
+                if(leftBound > input || input > rightBound){
+                    throw new InputOutOfBoundsException("Input is out of bounds");
+                }else
+                    System.out.println(input);
+            }catch (InputOutOfBoundsException e){
+                System.out.println(e.getMessage());
+            }catch (NumberFormatException e){
+                if(!nextLine.equals(terminationString))
+                    System.out.println(e.getMessage() + " is not a parsable integer");
+            }
+        }while (!nextLine.equals(terminationString));
+
     }
 }
--- src/com/clouway/SumStrings.java
+++ src/com/clouway/SumStrings.java
@@ -6,19 +6,21 @@ public class SumStrings {
 
     /**
      * Sums two numbers
+     * Rethrows possible exceptions with new message
      * @param a first number
      * @param b second number
      * @return float result casted to string
      */
-    public String sum(String a, String b){
-        String result = null;
+    public String sum(String a, String b) {
+        String result;
         try {
             result = String.valueOf(Float.parseFloat(a) + Float.parseFloat(b));
         } catch (NumberFormatException e){
-            System.err.println(e.getMessage()+" is not a parsable float");
+            throw new NumberFormatException(e.getMessage()+" is not a parsable float");
         } catch (NullPointerException e){
-            System.err.println("Input cannot be null");
+            throw new NullPointerException(String.format("a = %s and b = %s cannot be null",a,b));
         }
         return result;
+
     }
 }
